19d220aca3bc1e7e67a3314a187f595624fc2511
Nahom Teshome <noilnahoo@gmail.com>
2023-04-09 07:58:01 -0700

    this is a minor change on question number seven

--- FOP_GRP_WORK-GRP_06/project-work/Q#7/src/main.cpp
+++ FOP_GRP_WORK-GRP_06/project-work/Q#7/src/main.cpp
@@ -16,9 +16,7 @@ int main(){
     for (i = 0; i < 8; i++){
         cin >> date[i];
     }
-    for (i = 0; i < 8; i ++){
-        cout << date[i];
-    }
+    
     cout << endl;
     for (i = 0; i < 2; i++){
         day[i] = date [i];
