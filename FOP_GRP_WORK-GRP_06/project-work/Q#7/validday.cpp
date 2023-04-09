c69a59fa9a88e8494b903c1f622e1215193e3378
mindahun debebe <mndahun0@gmail.com>
2023-04-09 16:35:49 +0300

    coments edited

--- FOP_GRP_WORK-GRP_06/project-work/Q#7/validday.cpp
+++ FOP_GRP_WORK-GRP_06/project-work/Q#7/validday.cpp
@@ -22,115 +22,115 @@ int main()
                     leap = true;
             }
             else
-                leap = true; // check leap is finished
+                leap = true; // check leap year is finished hear
         bool valid = false;  // valid number of days
-        string invalid;      // this the message printed if the date is invalid
+        string invalid;      // this is
+        the message printed if the date is invalid
 
-        if (*date[1] > 0 && *date[1] <= 12 && *date[0] > 0)
+            if (*date[1] > 0 && *date[1] <= 12 && *date[0] > 0)
 
-            switch (*date[1])
+                switch (*date[1])
+        {
+        case 1:
+            if (*date[0] <= 31)
+                valid = true;
+            else
+                invalid = to_string(*date[0]) + " is invalid day!";
+            break;
+        case 2:
+            if (leap == true)
             {
-            case 1:
-                if (*date[0] <= 31)
+                if (*date[0] <= 29)
                     valid = true;
+                else if (*date[0] <= 31)
+                    invalid = to_string(*date[0]) + " is invalid day  in february!";
                 else
                     invalid = to_string(*date[0]) + " is invalid day!";
-                break;
-            case 2:
-                if (leap == true)
-                {
-                    if (*date[0] <= 29)
-                        valid = true;
-                    else if (*date[0] <= 31)
-                        invalid = to_string(*date[0]) + " is invalid day  in february!";
-                    else
-                        invalid = to_string(*date[0]) + " is invalid day!";
-                }
-                else
-                {
-                    if (*date[0] <= 28)
-                        valid = true;
-                    else if (*date[0] == 29)
-                        invalid = to_string(*date[0]) + " is invalid day  in february \nbecause the year is not leap year ";
-                    else if (*date[0] <= 31)
-                        invalid = *date[0] + " is invalid day  in february";
-                    else
-                        invalid = to_string(*date[0]) + " is invalid day";
-                }
-                break;
-            case 3:
-                if (*date[0] <= 31)
-                    valid = true;
-                else
-                    invalid = to_string(*date[0]) + " is invalid day";
-                break;
-            case 4:
-                if (*date[0] <= 30)
-                    valid = true;
-                else if (*date[0] == 31)
-                    invalid = to_string(*date[0]) + " is invalid day  in april";
-                else
-                    invalid = to_string(*date[0]) + " is invalid day";
-                break;
-            case 5:
-                if (*date[0] <= 31)
-                    valid = true;
-                else
-                    invalid = to_string(*date[0]) + " is invalid day";
-                break;
-            case 6:
-                if (*date[0] <= 30)
-                    valid = true;
-                else if (*date[0] == 31)
-                    invalid = to_string(*date[0]) + " is invalid day  in june";
-                else
-                    invalid = to_string(*date[0]) + " is invalid day";
-                break;
-            case 7:
-                if (*date[0] <= 31)
-                    valid = true;
-                else
-                    invalid = to_string(*date[0]) + " is invalid day";
-                break;
-            case 8:
-                if (*date[0] <= 31)
-                    valid = true;
-                else
-                    invalid = to_string(*date[0]) + " is invalid day";
-                break;
-            case 9:
-                if (*date[0] <= 30)
-                    valid = true;
-                else if (*date[0] == 31)
-                    invalid = to_string(*date[0]) + " is invalid day  in june";
-                else
-                    invalid = to_string(*date[0]) + " is invalid day";
-                break;
-            case 10:
-                if (*date[0] <= 31)
-                    valid = true;
-                else
-                    invalid = to_string(*date[0]) + " is invalid day";
-                break;
-            case 11:
-                if (*date[0] <= 30)
-                    valid = true;
-                else if (*date[0] == 31)
-                    invalid = to_string(*date[0]) + " is invalid day  in june";
-                else
-                    invalid = to_string(*date[0]) + " is invalid day";
-                break;
-            case 12:
-                if (*date[0] <= 31)
-                    valid = true;
-                else
-                    invalid = to_string(*date[0]) + " is invalid day";
-                break;
             }
+            else
+            {
+                if (*date[0] <= 28)
+                    valid = true;
+                else if (*date[0] == 29)
+                    invalid = to_string(*date[0]) + " is invalid day  in february \nbecause the year is not leap year ";
+                else if (*date[0] <= 31)
+                    invalid = *date[0] + " is invalid day  in february";
+                else
+                    invalid = to_string(*date[0]) + " is invalid day";
+            }
+            break;
+        case 3:
+            if (*date[0] <= 31)
+                valid = true;
+            else
+                invalid = to_string(*date[0]) + " is invalid day";
+            break;
+        case 4:
+            if (*date[0] <= 30)
+                valid = true;
+            else if (*date[0] == 31)
+                invalid = to_string(*date[0]) + " is invalid day  in april";
+            else
+                invalid = to_string(*date[0]) + " is invalid day";
+            break;
+        case 5:
+            if (*date[0] <= 31)
+                valid = true;
+            else
+                invalid = to_string(*date[0]) + " is invalid day";
+            break;
+        case 6:
+            if (*date[0] <= 30)
+                valid = true;
+            else if (*date[0] == 31)
+                invalid = to_string(*date[0]) + " is invalid day  in june";
+            else
+                invalid = to_string(*date[0]) + " is invalid day";
+            break;
+        case 7:
+            if (*date[0] <= 31)
+                valid = true;
+            else
+                invalid = to_string(*date[0]) + " is invalid day";
+            break;
+        case 8:
+            if (*date[0] <= 31)
+                valid = true;
+            else
+                invalid = to_string(*date[0]) + " is invalid day";
+            break;
+        case 9:
+            if (*date[0] <= 30)
+                valid = true;
+            else if (*date[0] == 31)
+                invalid = to_string(*date[0]) + " is invalid day  in june";
+            else
+                invalid = to_string(*date[0]) + " is invalid day";
+            break;
+        case 10:
+            if (*date[0] <= 31)
+                valid = true;
+            else
+                invalid = to_string(*date[0]) + " is invalid day";
+            break;
+        case 11:
+            if (*date[0] <= 30)
+                valid = true;
+            else if (*date[0] == 31)
+                invalid = to_string(*date[0]) + " is invalid day  in june";
+            else
+                invalid = to_string(*date[0]) + " is invalid day";
+            break;
+        case 12:
+            if (*date[0] <= 31)
+                valid = true;
+            else
+                invalid = to_string(*date[0]) + " is invalid day";
+            break;
+        }
         else if (*date[0] <= 0)
             invalid = to_string(*date[0]) + " is invalid day";
-        else
-            invalid = to_string(*date[1]) + " is invalid month";
+        else invalid = to_string(*date[1]) + " is invalid month";
         string NLyear;
         int temp = *date[2] + 4;
         if (valid == true)
@@ -139,11 +139,11 @@ int main()
             {
                 cout << " " + to_string(*date[0]) + "/" + to_string(*date[1]) + "/" + to_string(*date[2]) + " is valid and also leap year.\nhear is below the next twenty Leap years : \n";
 
-                for (int i = 1; i <= 20; i++)
+                for (int i = 1; i <= 20; i++) // this because if the year is less than 1000
                 {
                     switch (temp)
                     {
-                    case 1 ... 9:
+                    case 1 ... 9: // case 1 ... 9 means if year is from 1 up to 9
                         NLyear = "000" + to_string(temp);
                         break;
                     case 10 ... 99:
